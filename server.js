9117c612f06c78ed84fc49863407658db53740c0
French Mangigo <160458884+frenchmangigo@users.noreply.github.com>
2024-08-31 12:33:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY;
 
-// Endpoint to generate voice
+
 app.get('/voice-synthesis', async (req, res) => {
     const { id, text } = req.query;
 
@@ -15,18 +15,18 @@ app.get('/voice-synthesis', async (req, res) => {
     }
 
     try {
-        // Make a request to ElevenLabs Text-to-Speech API
+        
         const response = await axios.post('https://api.elevenlabs.io/v1/text-to-speech', 
         {
             text: text,
-            voice_id: id || "default_voice_id", // Replace with the desired voice ID or provide as a query parameter
+            voice_id: id || "pNInz6obpgDQGcFmaJgB", 
         }, 
         {
             headers: {
                 'Authorization': `Bearer ${ELEVENLABS_API_KEY}`,
                 'Content-Type': 'application/json',
             },
-            responseType: 'arraybuffer' // Important for audio data
+            responseType: 'arraybuffer' 
         });
 
         res.setHeader('Content-Type', 'audio/mpeg');
